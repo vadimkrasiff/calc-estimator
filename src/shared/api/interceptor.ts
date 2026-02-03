f4111f19ee1637f2a059c71aab61194b5c3e0719
Vadim <krasilkoffvad99@gmail.com>
2026-02-03 17:22:55 +0500

    add env

--- src/shared/api/interceptor.ts
+++ src/shared/api/interceptor.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Добавляем токен ко всем запросам
